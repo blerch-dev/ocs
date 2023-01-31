1d9bc1858475b63ea511e415c56cafd2e7c91742
blerch-dev <blerch.dev@gmail.com>
2023-01-31 09:58:57 -0600

    More State/Auth Functionality

--- client/src/routes/default.ts
+++ client/src/routes/default.ts
@@ -4,7 +4,11 @@ import { defaultLayout, defaultHead, embedComponent, chatComponent, headerCompon
 
 const DefaultRoute = new OCRoute({
     domain: '[\\s\\S]*',
-    callback: (router, options, setOption) => {
+    callback: (router, options, setOption, setSesh) => {
+        const setSessionData = (obj: string, key: string, value: any) => {
+            if(setSesh != undefined) setSesh(obj, key, value);
+        }
+
         router.get('/chat', (req, res, next) => {
             let head = defaultHead('OCS - Chat');
             let embed = embedComponent();
@@ -17,12 +21,42 @@ const DefaultRoute = new OCRoute({
             res.send(defaultLayout(head, body));
         });
 
+        router.post('/username', (req, res, next) => {
+            //console.log(req.body);
+            setSessionData('user', 'username', req.body.username);
+            res.status(200).end();
+        });
+
+        router.get('/username', (req, res, next) => {
+            let head = defaultHead('OCS');
+            let body = `
+                ${headerComponent('OCS Live', undefined, [{ label: 'bler.ch', link: 'https://bler.ch' }])}
+                <main>
+                    <form action="/username" method="post">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="username">
+                        <input type="submit" value="Submit">
+                    </form>
+                </main>
+            `;
+            res.send(defaultLayout(head, body));
+        });
+
         router.get('/', (req, res, next) => {
             let head = defaultHead('OCS');
             let body = `
                 ${headerComponent('OCS Live', undefined, [{ label: 'bler.ch', link: 'https://bler.ch' }])}
                 <main>
                     <h3>Home Page</h3>
+                    <p>Session</p>
+                    <pre>${JSON.stringify(options?.session ?? { error: 'No Session' }, null, 2)}</pre>
+                    <p>Cookies:</p>
+                    <pre>${options?.cookies ?? 'No Cookies'}</pre>
+                    <style>
+                        pre {
+                            margin-bottom: 10px;
+                        }
+                    </style>
                 </main>
             `;
 
