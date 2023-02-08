e71701e72c7f2c028c8f0cfb1714e689ce919a86
blerch-dev <blerch.dev@gmail.com>
2023-02-08 11:17:24 -0600

    Setting up DB Access

--- data/src/index.ts
+++ data/src/index.ts
@@ -1,4 +1,17 @@
 import { OCServer, OCRoute } from 'ocs-type';
+import { Pool } from 'pg';
+const supabase = require('../secrets/supabase.json');
+
+const pg = new Pool({
+    host: supabase.host,
+    user: supabase.user,
+    database: supabase.database,
+    password: supabase.password,
+    port: supabase.port,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+});
 
 const DefaultRoute = new OCRoute({
     domain: 'data.local',
